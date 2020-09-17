08a7247b36aa27dab88e3018a7cd4775365436d7
hugoprea <hugo.prea@coaxys.com>
2020-09-17 12:01:42 +0200

    Updates

---
+++ precache-manifest.807aa1df4daba7e423d362a08962015b.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b1a0cd765be3d7c2bde8f1c2e4c6f57",
+    "url": "/react_memory_game/index.html"
+  },
+  {
+    "revision": "90c8b6544693d6602a44",
+    "url": "/react_memory_game/static/css/main.1617b4d5.chunk.css"
+  },
+  {
+    "revision": "748d4bd698e7bdb3efd3",
+    "url": "/react_memory_game/static/js/2.933576c4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react_memory_game/static/js/2.933576c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90c8b6544693d6602a44",
+    "url": "/react_memory_game/static/js/main.f7d6e0a9.chunk.js"
+  },
+  {
+    "revision": "d2e44800865d5c93859a",
+    "url": "/react_memory_game/static/js/runtime-main.689a5e22.js"
+  },
+  {
+    "revision": "d4c16ed713024524cea53da1a1dd7fc5",
+    "url": "/react_memory_game/static/media/aiolia.d4c16ed7.jpg"
+  },
+  {
+    "revision": "a444e326691ceff27a62ec35ecb2787a",
+    "url": "/react_memory_game/static/media/aiolos.a444e326.jpg"
+  },
+  {
+    "revision": "b9b36fb9c7dbe1a4889b29c07299d847",
+    "url": "/react_memory_game/static/media/aldebaran.b9b36fb9.jpg"
+  },
+  {
+    "revision": "6e77a109a31e01ccead5b66ba95657ab",
+    "url": "/react_memory_game/static/media/aphrodite.6e77a109.jpg"
+  },
+  {
+    "revision": "973fe132387d60fa892616176d0fad9a",
+    "url": "/react_memory_game/static/media/athena.973fe132.jpg"
+  },
+  {
+    "revision": "1b7a62254072610aec2a228a7335ffab",
+    "url": "/react_memory_game/static/media/back_card.1b7a6225.png"
+  },
+  {
+    "revision": "754a3b224f73fd30eccadb769467f9a0",
+    "url": "/react_memory_game/static/media/back_card.754a3b22.jpg"
+  },
+  {
+    "revision": "84da3ca725ebe2efd7af2931e982b152",
+    "url": "/react_memory_game/static/media/bck.84da3ca7.png"
+  },
+  {
+    "revision": "c1c854a9d232b71854b998bf53612e74",
+    "url": "/react_memory_game/static/media/camus.c1c854a9.jpg"
+  },
+  {
+    "revision": "b18957d7d7e05eb70d6b623927c5a43f",
+    "url": "/react_memory_game/static/media/deathmask.b18957d7.jpg"
+  },
+  {
+    "revision": "0dd699efeb22a1dcd660939bb92df797",
+    "url": "/react_memory_game/static/media/doko.0dd699ef.jpg"
+  },
+  {
+    "revision": "7f34a6dea0eda41b43d16418993a4776",
+    "url": "/react_memory_game/static/media/evil_saga.7f34a6de.jpg"
+  },
+  {
+    "revision": "451e2d58cbf63afb41102f8afdd74ae8",
+    "url": "/react_memory_game/static/media/grand_pope.451e2d58.jpg"
+  },
+  {
+    "revision": "3daf21dcc09d04c2090f704180bf4bc7",
+    "url": "/react_memory_game/static/media/hades.3daf21dc.jpg"
+  },
+  {
+    "revision": "ae7c9366a77c1ff69950fa5c6edd88ad",
+    "url": "/react_memory_game/static/media/hypnos.ae7c9366.jpg"
+  },
+  {
+    "revision": "2dad1e69631ffd0cb1da30d994b71870",
+    "url": "/react_memory_game/static/media/ikki_gc.2dad1e69.jpg"
+  },
+  {
+    "revision": "9275013cf9da48100265093fb1103640",
+    "url": "/react_memory_game/static/media/milo.9275013c.jpg"
+  },
+  {
+    "revision": "1ca5501a2a8563eaec108006a90f635f",
+    "url": "/react_memory_game/static/media/mu.1ca5501a.jpg"
+  },
+  {
+    "revision": "dbfa3a6be695d3b14f90de18b8a82f8c",
+    "url": "/react_memory_game/static/media/poseidon.dbfa3a6b.jpg"
+  },
+  {
+    "revision": "9900251bf948edc9e992438decafccb9",
+    "url": "/react_memory_game/static/media/saga.9900251b.jpg"
+  },
+  {
+    "revision": "1b6e700b2f941b000d5c3a891cc9bda3",
+    "url": "/react_memory_game/static/media/seiya_gc.1b6e700b.jpg"
+  },
+  {
+    "revision": "facc1e4feecf7a66460049cbb1580c45",
+    "url": "/react_memory_game/static/media/shaka.facc1e4f.jpg"
+  },
+  {
+    "revision": "b23c8d3f27071e76abb35f95832549fc",
+    "url": "/react_memory_game/static/media/shaka_arayashiki.b23c8d3f.jpg"
+  },
+  {
+    "revision": "09ad21e4ae98ad600147f67808f43e92",
+    "url": "/react_memory_game/static/media/shion.09ad21e4.jpg"
+  },
+  {
+    "revision": "ab9b9fdef8038b826615be569f2449d2",
+    "url": "/react_memory_game/static/media/shiryu_gc.ab9b9fde.jpg"
+  },
+  {
+    "revision": "83a1ef88f247dfebbbda79927e5c4bdb",
+    "url": "/react_memory_game/static/media/shun_gc.83a1ef88.jpg"
+  },
+  {
+    "revision": "182c41dba99e3eaa7ba9ccb585d7203c",
+    "url": "/react_memory_game/static/media/shura.182c41db.jpg"
+  }
+]);
